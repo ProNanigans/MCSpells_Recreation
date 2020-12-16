3f8fd028bb218c72569f29d422f1026a7f0a168d
Daniel <22dawitd@mydacc.org>
2020-12-16 08:47:09 -0500

    Add Mana Cost to spell enums

--- src/main/java/me/nanigans/potterworldspells/Utils/Spells.java
+++ src/main/java/me/nanigans/potterworldspells/Utils/Spells.java
@@ -2,29 +2,32 @@ package me.nanigans.potterworldspells.Utils;
 
 public enum Spells {
 
-    ANTI_APPARATE("Anti Apparate", 2, "CrowdControl"),//name, custom model data
-    ACCIO("Accio", 3, "CrowdControl"),
-    APPARATE("Apparate", 9, "Mobility"),
-    ASCENDIO("Ascendio", 14, "Mobility"),
-    TRIPUDIO("Tripudo", 181, "Mobility"),
-    VOLO("Volo", 182, "Mobility"),
-    EPISKEY("Episkey", 55, "Healing"),
-    FERUVIO("Feruvio", 141, "Healing"),
-    VULNERA("Vulnera", 140, "Healing"),
-    ARRESTO_MOMENTUM("Arresto Momentum", 15, "Defensive"),
-    DISAPPEARUS("Disappearus", 43, "Defensive"),
-    FORTIFICUS("Fortificus", 183, "Defensive"),
-    FINITE_INCANTATEM("Finite Incantatem", 60, "Defensive"),
-    FLAME_FREEZE("Flame Freeze", 56, "Defensive"),
-    AGUAMENTI("Aguamenti", 4, "Combat");
+    ANTI_APPARATE("Anti Apparate", 2, "CrowdControl", 20),//name, custom model data
+    ACCIO("Accio", 3, "CrowdControl", 0),
+    APPARATE("Apparate", 9, "Mobility", 0),
+    ASCENDIO("Ascendio", 14, "Mobility", 0),
+    TRIPUDIO("Tripudo", 181, "Mobility", 0),
+    VOLO("Volo", 182, "Mobility", 0),
+    EPISKEY("Episkey", 55, "Healing", 0),
+    FERUVIO("Feruvio", 141, "Healing", 0),
+    VULNERA("Vulnera", 140, "Healing", 0),
+    ARRESTO_MOMENTUM("Arresto Momentum", 15, "Defensive", 0),
+    DISAPPEARUS("Disappearus", 43, "Defensive", 0),
+    FORTIFICUS("Fortificus", 183, "Defensive", 0),
+    FINITE_INCANTATEM("Finite Incantatem", 60, "Defensive", 0),
+    FLAME_FREEZE("Flame Freeze", 56, "Defensive", 0),
+    AGUAMENTI("Aguamenti", 4, "Combat", 0);
     String name, spellType;
     int data;
+    int cost;
 
-    Spells(String name, int modelData, String spellType) {
+    Spells(String name, int modelData, String spellType, int manaCost) {
         this.name = name;
         this.data = modelData;
         this.spellType = spellType;
     }
+
+    public int getCost(){return this.cost;}
 
     public String getSpellType() {
         return spellType;
